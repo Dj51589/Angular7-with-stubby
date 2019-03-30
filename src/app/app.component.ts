a7e688f7710c1f8fe19d81e576f7e318b16421d9
Dheeraj Jaiswal <jdheeraj32@gmail.com>
2019-03-30 14:09:52 +0400

    stubby integrated with concurrently to run local dev and stubby server together

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Employye } from '../app/employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular7-with-stubby';
+  SERVER_URL = environment.server_url;
+  employeeList: Employye[];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchEmployeeList();
+  }
+
+  fetchEmployeeList() {
+    const url = this.SERVER_URL + '/employeesList';
+    this.http.get(url).subscribe((response: Employye[]) => {
+      this.employeeList = response;
+    });
+  }
 }
---
+++ src/app/employee.ts
@@ -0,0 +1,6 @@
+export class Employye {
+    first_name: string;
+    last_name: string;
+    age: number;
+    designation: string;
+}
